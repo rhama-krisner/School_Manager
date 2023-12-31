b41237d1e1e170015c4e3d95822622461a7af622
Rhama Krisner <rhamakrisner@gmail.com>
2023-12-31 15:02:15 -0300

    Create project

---
+++ src/test/java/com/schoolmanager/school_manager/SchoolManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanager.school_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
