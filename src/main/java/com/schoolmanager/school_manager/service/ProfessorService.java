5e44ccc43fee032c4794e7b918d94a2c07f94489
Rhama Krisner <rhamakrisner@gmail.com>
2024-01-03 14:27:25 -0300

    Adicionado Service para professor

--- src/main/java/com/schoolmanager/school_manager/service/ProfessorService.java
+++ src/main/java/com/schoolmanager/school_manager/service/ProfessorService.java
@@ -2,7 +2,8 @@ package com.schoolmanager.school_manager.service;
 
 import com.schoolmanager.school_manager.exceptions.ResourceNotFoundException;
 import com.schoolmanager.school_manager.model.Aluno;
-import com.schoolmanager.school_manager.repository.AlunoRepository;
+import com.schoolmanager.school_manager.model.Professor;
+import com.schoolmanager.school_manager.repository.ProfessorRepository;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Method;
@@ -10,39 +11,38 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class AlunoService {
-    final AlunoRepository repository;
-
-    public AlunoService(AlunoRepository repository) {
+public class ProfessorService {
+    final ProfessorRepository repository;
+    public ProfessorService(ProfessorRepository repository) {
         this.repository = repository;
     }
 
-    public Aluno findById(Long id) {
-        Optional<Aluno> obj = repository.findById(id);
-        return obj.orElseThrow(() -> new ResourceNotFoundException(
-                "Objeto não encontrado! Id: " + id + ", Tipo: " + Aluno.class.getName())
+    public Professor findById(Long id){
+        Optional<Professor> obj = repository.findById(id);
+        return obj.orElseThrow(()->new ResourceNotFoundException(
+                "Objeto não encontrado! ID: " + id + ", Tipo: " + Professor.class.getName())
         );
     }
 
-    public List<Aluno> findAll() {
+    public List<Professor> findAll() {
         return repository.findAll();
     }
 
-    public Aluno create(Aluno aluno) {
-        return repository.save(aluno);
+    public Professor create(Professor professor) {
+        return repository.save(professor);
     }
 
-    public Aluno update(Aluno aluno) {
-        Aluno repo = repository.findById(aluno.getId()).orElseThrow(
+    public Professor update(Professor professor) {
+        Professor repo = repository.findById(professor.getId()).orElseThrow(
                 () -> new ResourceNotFoundException("Não foi encontrado pessoas com esse ID"));
 
-        Class<? extends Aluno> clazz = repo.getClass();
+        Class<? extends Professor> clazz = repo.getClass();
         for (Method method : clazz.getMethods()) {
             if (method.getName().startsWith("set")) {
                 try {
                     String methodName = method.getName().substring(3);
                     Method getMethod = clazz.getMethod("get" + methodName);
-                    Object value = getMethod.invoke(aluno);
+                    Object value = getMethod.invoke(professor);
                     method.invoke(repo, value);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
@@ -54,10 +54,9 @@ public class AlunoService {
     }
 
     public void delete(Long id) {
-        Aluno entity = repository.findById(id).orElseThrow(() ->
+        Professor entity = repository.findById(id).orElseThrow(() ->
                 new ResourceNotFoundException("Sem dados encontrados para esse ID"));
         repository.delete(entity);
     }
 
 }
-
